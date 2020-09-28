b6086b8a8f97ca8f585fe759b6424a8976396a60
Stepan <stepansmetanskyy@gmail.com>
2020-09-28 23:00:34 +0300

    project from https://devcolibri.com/getting-started-with-retrofit-in-android/

--- app/src/main/java/com/retrofittestforstudying/MainActivity.java
+++ app/src/main/java/com/retrofittestforstudying/MainActivity.java
@@ -1,8 +1,17 @@
 package com.retrofittestforstudying;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.retrofittestforstudying.models.Post;
+import com.retrofittestforstudying.network.NetworkService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = findViewById(R.id.textView);
+
+        NetworkService.getInstance()
+                .getJSONApi()
+                .getPostWithID(1)
+                .enqueue(new Callback<Post>() {
+                    @Override
+                    public void onResponse(@NonNull Call<Post> call, @NonNull Response<Post> response) {
+                        Post post = response.body();
+
+                        assert post != null;
+                        textView.append("\n");
+                        textView.append(post.getId() + "\n\n");
+                        textView.append(post.getUserId() + "\n\n");
+                        textView.append(post.getTitle() + "\n\n");
+                        textView.append(post.getBody() + "\n");
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull Call<Post> call, @NonNull Throwable t) {
+
+                        textView.append("Error occurred while getting request!");
+                        t.printStackTrace();
+                    }
+                });
     }
 }
