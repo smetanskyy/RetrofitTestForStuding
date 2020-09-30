de3e5a595dfafe7fd6f97d82fecb5caa0070834f
Stepan <stepansmetanskyy@gmail.com>
2020-10-01 01:09:08 +0300

    add design

--- app/src/main/java/com/retrofittestforstudying/MainActivity.java
+++ app/src/main/java/com/retrofittestforstudying/MainActivity.java
@@ -1,6 +1,9 @@
 package com.retrofittestforstudying;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickBtn(View view) {
+        TextView tw = findViewById(R.id.textView);
+
+        try {
+            Button btn = findViewById(view.getId());
+            if (tw.getText().toString().equals("0")) {
+                tw.setText(btn.getText().toString());
+            }
+
+        } catch (Exception ignored) {
+        }
     }
 }
