5c136e4dda41c92146d29b4ffb5d9d4e1b2e556b
Stepan <stepansmetanskyy@gmail.com>
2020-09-30 23:05:38 +0300

    start Simple Calculator

--- app/src/main/java/com/retrofittestforstudying/MainActivity.java
+++ app/src/main/java/com/retrofittestforstudying/MainActivity.java
@@ -1,21 +1,8 @@
 package com.retrofittestforstudying;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.widget.TextView;
 
-import com.retrofittestforstudying.models.Currency;
-import com.retrofittestforstudying.network.NetworkService;
-
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,36 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final TextView textView = findViewById(R.id.textView);
-
-        NetworkService.getInstance()
-                .getJSONApi()
-                .getAllCurrency()
-                .enqueue(new Callback<List<Currency>>() {
-                    @RequiresApi(api = Build.VERSION_CODES.N)
-                    @Override
-                    public void onResponse(@NonNull Call<List<Currency>> call, @NonNull Response<List<Currency>> response) {
-                        textView.append("\n");
-                        List<Currency> currencies = response.body();
-                        assert currencies != null;
-                        currencies.forEach(currency -> {
-                                    textView.append("\nCurrency " + currency.getCcy() + "\n");
-                                    textView.append(String.format("BUY: 1 %s -> %s %s\n", currency.getCcy(),
-                                            currency.getBuy(), currency.getBase_ccy()));
-                                    textView.append(String.format("SALE: 1 %s -> %s %s\n", currency.getCcy(),
-                                            currency.getSale(), currency.getBase_ccy()));
-                                }
-                        );
-                        textView.append("\n");
-                    }
-
-                    @Override
-                    public void onFailure(@NonNull Call<List<Currency>> call, @NonNull Throwable t) {
-
-                        textView.append("Error occurred while getting request!");
-                        t.printStackTrace();
-                    }
-                });
     }
 }
